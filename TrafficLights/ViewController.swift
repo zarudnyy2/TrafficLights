5d1e573045d2a0c5746d0f2f05634526ce36f7a5
Aleksey Zarudny <zarudnyy2@gmail.com>
2022-03-03 16:13:07 +0300

    logic app

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -18,14 +18,28 @@ class ViewController: UIViewController {
         redLight.alpha = 0.3
         yellowLight.alpha = 0.3
         greenLight.alpha = 0.3
+        
         lightButton.layer.cornerRadius = 20
-        redLight.layer.cornerRadius = 75
-        yellowLight.layer.cornerRadius = 75
-        greenLight.layer.cornerRadius = 75
-    }
-
-    @IBAction func changeLightButton() {
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
     }
     
+    @IBAction func changeLightButton() {
+        lightButton.setTitle("NEXT", for: .normal)
+        if redLight.alpha != 1 && yellowLight.alpha != 1 && greenLight.alpha != 1 {
+            redLight.alpha = 1
+        } else if redLight.alpha == 1 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+        } else if yellowLight.alpha == 1 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+        } else { yellowLight.alpha = 0.3
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+        }
+    }
 }
 
